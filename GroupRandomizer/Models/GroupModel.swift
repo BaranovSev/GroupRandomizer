083b97f846992177ff8ed18f76f112d328d83e40
BaranovSev <aldebaran94sev@gmail.com>
2023-10-04 16:07:30 +0300

    GroupModel commited

---
+++ GroupRandomizer/Models/GroupModel.swift
@@ -0,0 +1,20 @@
+//
+//  GroupModel.swift
+//  GroupRandomizer
+//
+//  Created by Stepan Baranov on 04.10.2023.
+//
+
+import Foundation
+
+struct Group: Codable {
+    var groupName: String
+    var groupTask: String?
+    var students: [Student]
+    
+    init(groupName: String, groupInfo: String? = nil, students: [Student]) {
+        self.groupName = groupName
+        self.groupTask = groupInfo
+        self.students = students
+    }
+}
